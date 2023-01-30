31352d3e5f896fd22d09b91aa13e705d9e75434b
Murat <muratdurmaz654@gmail.com>
2023-01-29 23:35:16 -0800

    Added more hello feature

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Work1 started.");
         System.out.println("Created git repository");
+        System.out.println("local repo is already published to Github");
+        System.out.println("doing more work");
     }
 }
